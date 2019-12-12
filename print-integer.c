fadcf9532f0e5449cffe667400803c4d96c41491
Vaibhav Mishra <mishra.vaibhav6@gmail.com>
2019-12-12 17:08:51 +0530

    Update print-integer.c

--- print-integer.c
+++ print-integer.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
  
-main()
+ void main()
 {
     int a;
  // from Kuba-PC
     printf("Enter an integer\n");
     scanf("%d", &a);
+    printf("Integer that you have entered is %d", a);
  
-    printf("Integer that you have entered is %d\n", a);
- 
-    return 0;
 }
